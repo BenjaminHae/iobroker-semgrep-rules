46541c09e75c6d9c77c3448c070885c7daa79429
Benjamin Häublein <benjaminhaeublein@gmail.com>
2023-01-02 11:17:17 +0100

    add rules and script

---
+++ generateDocs.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+SCRIPT_PATH="scripts/"
+OUT_PATH="docs/files.md"
+
+if [ "$#" -ge 1 ]; then
+    SCRIPT_PATH="${1}"
+fi
+if [ "$#" -ge 2 ]; then
+    DOC_PATH="${2}"
+fi
+
+SCRIPT_PATH=$(realpath "${SCRIPT_PATH}")
+OUT_PATH=$(realpath "${OUT_PATH}")
+
+echo "" > ${OUT_PATH}
+
+# title, filter
+print_result() {
+    LIST=$(echo "$SEMRES" | grep " $2 " | sed 's/^\s*\S*\s\S*\s\(.*\)$/ - \1/' | sort | uniq)
+    if [ -z "$LIST" ]
+    then
+        echo "$1 is empty"
+    else
+        echo "### $1" >> ${OUT_PATH}
+        echo "" >> ${OUT_PATH}
+        echo "$LIST" | sort | uniq >> ${OUT_PATH}
+        echo "" >> ${OUT_PATH}
+    fi
+}
+
+cd "${SCRIPT_PATH}"
+
+# todo use find instead of loop
+for f in */; do
+  cd $f;
+  for t in *; do
+    echo "## $f$t" >> ${OUT_PATH}
+    echo "" >> ${OUT_PATH}
+    # todo use json output
+    SEMRES=$(semgrep --config=../../rules.yml "${t}" 2> /dev/null | sed "s/^\s*//;s/'//g;s/\"//g" | grep iob.js)
+
+    print_result Subscriptions on
+    print_result "Set State" setState
+    print_result "Get State" getState
+    print_result "Schedule" schedule
+
+  done
+  cd ..
+done;
